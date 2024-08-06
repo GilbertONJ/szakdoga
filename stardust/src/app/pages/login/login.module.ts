bd02345bd36dfae98dce157d950c746ae4d96cb9
Kovács Gilbert <h160307@stud.u-szeged.hu>
2024-08-06 15:40:27 +0200

    feat: Create login form

--- stardust/src/app/pages/login/login.module.ts
+++ stardust/src/app/pages/login/login.module.ts
@@ -4,12 +4,22 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
     CommonModule,
     LoginRoutingModule
   ]
