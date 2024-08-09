d29fa919fb557fd6040129b8aa6bf6bf685903fd
Kovács Gilbert <h160307@stud.u-szeged.hu>
2024-08-09 18:24:46 +0200

    fix: responsivity of sign in and up pages

--- stardust/src/app/app-routing.module.ts
+++ stardust/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+  { path: '', loadChildren: () => import('./pages/sign-up/sign-up.module').then(m => m.SignUpModule) },
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
   { path: 'sign-up', loadChildren: () => import('./pages/sign-up/sign-up.module').then(m => m.SignUpModule) }
 ];
